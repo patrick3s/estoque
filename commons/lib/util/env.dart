144242fd39479a4edce4c616bdc15fcfaf253618
Patrick <tk_patrick@hotmail.com>
2021-10-10 16:38:47 -0300

    env atualizado

--- commons/lib/util/env.dart
+++ commons/lib/util/env.dart
@@ -1,6 +1,6 @@
 class EnvApp {
   static const appName = String.fromEnvironment('DEFINE_APP_NAME');
-  static const suffixName = String.fromEnvironment('DEFINE_SUFFIX_NAME');
+  static const suffixName = String.fromEnvironment('DEFINE_SUFFIX_NAME',defaultValue: 'prod');
   static const baseApi = String.fromEnvironment('DEFINE_BASE_API',defaultValue: "https://api-meu-nogocio.herokuapp.com/"  );
   static const baseServer = String.fromEnvironment('DEFINE_SERVER');
 }
